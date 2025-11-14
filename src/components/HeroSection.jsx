5dc637ac36538532abd7033024b341bc63c57896
Alan Hamid <alanhamid13@gmail.com>
2025-11-13 21:47:26 -0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import heroBg from ;
 
 const HeroWrapper = styled.section`
   background: url(${heroBg}) no-repeat center center/cover;
